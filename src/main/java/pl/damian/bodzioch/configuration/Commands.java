8cff41aad03c18a9383438c9909a04384091a0f8
OpusMagnus5 <damianbodzioch95@gmail.com>
2023-02-04 14:16:41 +0100

    dodanie komendy speed

--- src/main/java/pl/damian/bodzioch/configuration/Commands.java
+++ src/main/java/pl/damian/bodzioch/configuration/Commands.java
@@ -12,13 +12,20 @@ import java.util.List;
 public class Commands {
 
     public static final String HERO_COMMAND = "hero";
-    public static final String BOHATER_COMMAND_OPTION = "bohater";
+    public static final String BOHATER_HERO_COMMAND_OPTION = "bohater";
     public static final String KALENDARZ_COMMAND = "kalendarz";
+    public static final String SPEED_COMMAND = "speed";
+    public static final String BASE_OPTION_SPEED_COMMAND = "baza";
+    public static final String SIATKA_OPTION_SPEED_COMMAND = "siatka";
+    public static final String GUN_OPTION_SPEED_COMMAND = "bron";
+    public static final String FAMILY_OPTION_SPEED_COMMAND = "rodzina";
+    public static final String SKILL_OPTION_SPEED_COMMAND = "skill";
 
     public void setAllCommands(GatewayDiscordClient client) {
         List<ApplicationCommandRequest> commands = new ArrayList<>();
         commands.add(setHeroCommand());
         commands.add(setKalendarzCommand());
+        commands.add(setSpeedCommand());
         activateCommand(commands, client);
     }
 
@@ -27,7 +34,7 @@ public class Commands {
                 .name(HERO_COMMAND)
                 .description("Wyświetla zdjęcie bohatera.")
                 .addOption(ApplicationCommandOptionData.builder()
-                        .name(BOHATER_COMMAND_OPTION)
+                        .name(BOHATER_HERO_COMMAND_OPTION)
                         .description("Nazwa bohatera")
                         .type(ApplicationCommandOption.Type.STRING.getValue())
                         .required(true)
@@ -40,6 +47,72 @@ public class Commands {
         return ApplicationCommandRequest.builder()
                 .name(KALENDARZ_COMMAND)
                 .description("Wyświetla aktualny kalendarz.")
+                .build();
+    }
+
+    private ApplicationCommandRequest setSpeedCommand() {
+        return ApplicationCommandRequest.builder()
+                .name(SPEED_COMMAND)
+                .description("Sprawdza czy uda się przełamać szybkość bohatera")
+                .addAllOptions(List.of(buildBaseOptionSpeedCommand(), buildSiatkaBonusOptionSpeedCommand(),
+                        buildGunBonusOptionSpeedCommand(), buildFamilyBonusOptionSpeedCommand(), buildSkillBonusOptionSpeedCommand()))
+                .build();
+    }
+
+    private ImmutableApplicationCommandOptionData buildBaseOptionSpeedCommand() {
+        return ApplicationCommandOptionData.builder()
+                .name(BASE_OPTION_SPEED_COMMAND)
+                .description("Podaj szybkość bazową karty.")
+                .type(ApplicationCommandOption.Type.INTEGER.getValue())
+                .required(true)
+                .minValue(1D)
+                .maxValue(100D)
+                .build();
+    }
+
+    private ImmutableApplicationCommandOptionData buildSiatkaBonusOptionSpeedCommand() {
+        return ApplicationCommandOptionData.builder()
+                .name(SIATKA_OPTION_SPEED_COMMAND)
+                .description("Podaj procent do szybkości ze siatki")
+                .type(ApplicationCommandOption.Type.INTEGER.getValue())
+                .choices(List.of(ApplicationCommandOptionChoiceData.builder()
+                                .name("2%")
+                                .value(2)
+                                .build(),
+                        ApplicationCommandOptionChoiceData.builder()
+                                .name("4%")
+                                .value(4)
+                                .build()))
+                .build();
+    }
+
+    private ImmutableApplicationCommandOptionData buildGunBonusOptionSpeedCommand() {
+        return ApplicationCommandOptionData.builder()
+                .name(GUN_OPTION_SPEED_COMMAND)
+                .description("Podaj bonus szybkości broni")
+                .type(ApplicationCommandOption.Type.INTEGER.getValue())
+                .minValue(0D)
+                .maxValue(9D)
+                .build();
+    }
+
+    private ImmutableApplicationCommandOptionData buildFamilyBonusOptionSpeedCommand() {
+        return ApplicationCommandOptionData.builder()
+                .name(FAMILY_OPTION_SPEED_COMMAND)
+                .description("Podaj bonus szybkości rodziny")
+                .type(ApplicationCommandOption.Type.INTEGER.getValue())
+                .minValue(0D)
+                .maxValue(100D)
+                .build();
+    }
+
+    private ImmutableApplicationCommandOptionData buildSkillBonusOptionSpeedCommand() {
+        return ApplicationCommandOptionData.builder()
+                .name(SKILL_OPTION_SPEED_COMMAND)
+                .description("Podaj bonus szybkości umiejętności")
+                .type(ApplicationCommandOption.Type.INTEGER.getValue())
+                .minValue(0D)
+                .maxValue(100D)
                 .build();
     }
 
--- src/main/java/pl/damian/bodzioch/eventListeners/ChatInputInteractionEventListener.java
+++ src/main/java/pl/damian/bodzioch/eventListeners/ChatInputInteractionEventListener.java
@@ -7,6 +7,7 @@ import discord4j.core.object.component.ActionRow;
 import discord4j.core.object.component.Button;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.core.spec.InteractionApplicationCommandCallbackSpec;
+import discord4j.rest.util.Color;
 import org.springframework.stereotype.Service;
 import pl.damian.bodzioch.configuration.Commands;
 import pl.damian.bodzioch.fileService.FileConstans;
@@ -29,6 +30,8 @@ public class ChatInputInteractionEventListener implements EventListener<ChatInpu
                 return event.reply(buildRespondForHeroCommand(event)).onErrorResume(this::handleError);
             case "kalendarz" :
                 return event.reply(buildRespondForKalendarzCommand(event)).onErrorResume(this::handleError);
+            case "speed" :
+                return event.reply(buildRespondForSpeedCommand(event)).onErrorResume(this::handleError);
             default:
                 return Mono.empty();
         }
@@ -40,7 +43,7 @@ public class ChatInputInteractionEventListener implements EventListener<ChatInpu
     }
 
     private InteractionApplicationCommandCallbackSpec buildRespondForHeroCommand(ChatInputInteractionEvent event) {
-        String heroName = event.getOption(Commands.BOHATER_COMMAND_OPTION)
+        String heroName = event.getOption(Commands.BOHATER_HERO_COMMAND_OPTION)
                 .flatMap(ApplicationCommandInteractionOption::getValue)
                 .map(ApplicationCommandInteractionOptionValue::asString).orElse("");
 
@@ -68,5 +71,35 @@ public class ChatInputInteractionEventListener implements EventListener<ChatInpu
                                 + Commands.KALENDARZ_COMMAND + FileConstans.JPG_FILE_EXTENSION)
                         .build())
                 .build();
+    }
+
+    private InteractionApplicationCommandCallbackSpec buildRespondForSpeedCommand(ChatInputInteractionEvent event) {
+        long base = event.getOption(Commands.BASE_OPTION_SPEED_COMMAND).flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong).orElse(0L);
+        long gun = event.getOption(Commands.GUN_OPTION_SPEED_COMMAND).flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong).orElse(0L);
+        long siatka = event.getOption(Commands.SIATKA_OPTION_SPEED_COMMAND).flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong).orElse(0L);
+        long family = event.getOption(Commands.FAMILY_OPTION_SPEED_COMMAND).flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong).orElse(0L);
+        long skill = event.getOption(Commands.SKILL_OPTION_SPEED_COMMAND).flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong).orElse(0L);
+
+        return InteractionApplicationCommandCallbackSpec.builder()
+                .addEmbed(EmbedCreateSpec.builder()
+                        .color(Color.GREEN)
+                        .addField("Bazowa prędkość", String.valueOf(base), false)
+                        .addField("Prędkość broni", String.valueOf(gun), false)
+                        .addField("Bonus z siatki", siatka + "%", false)
+                        .addField("Bonus rodziny", family + "%", false)
+                        .addField("Bonus z umiejętności", skill + "%", false)
+                        .addField("Liczba klocków bez bonusów", calculateSpeed(base, 0L, 0L, 0L, 0L), false)
+                        .addField("Liczba klocków z bonusami", calculateSpeed(base, gun, siatka, family, skill), false)
+                        .build())
+                .build();
+    }
+
+    private String calculateSpeed(long base, long gun, long siatka, long family, long skill){
+        return String.valueOf((int) Math.ceil((((78 - (base + gun)) * 0.14D) + 6.08D) / (1 + ((siatka + family + skill) / 100D))));
     }
 }
