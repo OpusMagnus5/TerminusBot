f717c3c44896c18aa182be17fc94d3dbc01649f8
Szaddaj <szaddaj@gmail.com>
2023-04-02 12:12:32 +0200

    # testowanie rozwiązan z schedulingiem

--- src/main/java/pl/damian/bodzioch/configuration/BotConfiguration.java
+++ src/main/java/pl/damian/bodzioch/configuration/BotConfiguration.java
@@ -5,6 +5,7 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -20,13 +21,16 @@ public class BotConfiguration {
     private final String TOKEN = "MTA2ODQ5NjQwNTUxMTAyMDU5NA.GgmBbO.yPt19IsVdnDp2-A9vjrsnCkOM_cXSxdgBxnA1s";
     public static final long applicationId = 1068496405511020594L;
 
+    @Autowired
+    ListService listService;
+
     @Bean
     public <T extends Event> GatewayDiscordClient getBotClient(List<EventListener<T>> eventListeners) {
         final DiscordClient client = DiscordClient.create(TOKEN);
         final GatewayDiscordClient gateway = client.login().block();
         Commands.setAllCommands(gateway);
         logger().info("Aktywowano komendy");
-        ListService.updateAllLists();
+        listService.updateAllLists();
         logger().info("Zaktualizowano listy");
         for (EventListener<T> listener : eventListeners) {
             gateway.on(listener.getEventType()).flatMap(listener::processCommand).subscribe();
@@ -39,6 +43,5 @@ public class BotConfiguration {
     public Logger logger() {
         return LoggerFactory.getLogger("pl.damian.bodzioch");
     }
-
 
 }
--- src/main/java/pl/damian/bodzioch/fileService/ListService.java
+++ src/main/java/pl/damian/bodzioch/fileService/ListService.java
@@ -1,6 +1,8 @@
 package pl.damian.bodzioch.fileService;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,6 +12,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Lazy(value = false)
+@Component
 public class ListService {
     public static final String RESOURCE_DIR = "/home/ubuntu/TerminusBot/resources/";
     public static final String HERO_DIR = "hero/";
@@ -19,8 +23,14 @@ public class ListService {
     public static final String CALENDAR_DIR = "kalendarz/";
     public static final String JPG_FILE_EXTENSION = ".jpg";
 
+    public void updateAllLists() {
+        DataInLists.HERO_NAMES = ListService.updateList(ListService.HERO_DIR);
+        DataInLists.HERO_SIATKA_LIST = ListService.updateList(ListService.SIATKA_DIR);
+        DataInLists.HERO_WARIANT_LIST = ListService.updateList(ListService.WARIANT_DIR);
+        DataInLists.SIATKI_WARIANTOW_LIST = ListService.updateList(ListService.SIATKA_WARIANTU_DIR);
+    }
 
-    public static List<String> updateList(String dir) {
+    private static List<String> updateList(String dir) {
         try (Stream<Path> stream = Files.list(Paths.get(RESOURCE_DIR + dir))) {
             return stream
                     .filter(file -> !Files.isDirectory(file))
@@ -34,11 +44,8 @@ public class ListService {
         }
     }
 
-    @Scheduled(cron = "@daily")
-    public static void updateAllLists() {
-        DataInLists.HERO_NAMES = ListService.updateList(ListService.HERO_DIR);
-        DataInLists.HERO_SIATKA_LIST = ListService.updateList(ListService.SIATKA_DIR);
-        DataInLists.HERO_WARIANT_LIST = ListService.updateList(ListService.WARIANT_DIR);
-        DataInLists.SIATKI_WARIANTOW_LIST = ListService.updateList(ListService.SIATKA_WARIANTU_DIR);
+    @Scheduled(cron = "0 0 0 * * *")
+    public void scheduledUpdateAllLists(){
+        updateAllLists();
     }
 }
